0dafba704ae37b4c75e92856427e41201e2f1744
jvtaylar-cpe <90173488+jvtaylar-cpe@users.noreply.github.com>
2023-05-12 21:48:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,108 @@
-import streamlit as st
-import numpy as np
+import sqlite3 
+import tkinter as tk
 
-# Create a header for the calculator.
-st.header("Scientific Calculator")
-
-# Create a function to perform a mathematical operation on two numbers.
-def calculate(operation, number1, number2):
-    if operation == "+":
-        return number1 + number2
-    elif operation == "-":
-        return number1 - number2
-    elif operation == "*":
-        return number1 * number2
-    elif operation == "/":
-        return number1 / number2
-    elif operation == "^":
-        return np.power(number1, number2)
-    elif operation == "log":
-        return np.log(number1)
-    elif operation == "sin":
-        return np.sin(number1)
-    elif operation == "cos":
-        return np.cos(number1)
-    elif operation == "tan":
-        return np.tan(number1)
-    elif operation == "sqrt":
-        return np.sqrt(number1)
-    else:
-        raise ValueError("Invalid operation.")
-
-# Create a number input for the first number.
-number1 = st.number_input("Enter the first number:")
-
-# Create a number input for the second number.
-number2 = st.number_input("Enter the second number:")
-
-# Create a dropdown menu for the mathematical operation.
-operation = st.selectbox("Select a mathematical operation:", ["+", "-", "*", "/", "^", "log", "sin", "cos", "tan", "sqrt"])
-
-# Calculate the result of the mathematical operation.
-result = calculate(operation, number1, number2)
-
-# Display the result of the mathematical operation.
-st.write("The result is", result)
+class CRUDGUI:
+    def __init__(self,master):
+        self.master = master
+        self.master.title("CRUD Database App")
+        
+        #create UI elements
+        self.name_label = tk.Label(self.master, text="Name:")
+        self.name_label.grid(row=0, column=0, padx=5, pady=5)
+        self.name_entry = tk.Entry(self.master)
+        self.name_entry.grid(row=0, column=1, padx=5, pady=5)
+        
+        self.email_label = tk.Label(self.master, text="Email:")
+        self.email_label.grid(row=1, column=0, padx=5, pady=5)
+        self.email_entry = tk.Entry(self.master)
+        self.email_entry.grid(row=1, column=1, padx=5, pady=5)
+        
+        self.phone_label = tk.Label(self.master, text="Phone:")
+        self.phone_label.grid(row=2, column=0, padx=5, pady=5)
+        self.phone_entry = tk.Entry(self.master)
+        self.phone_entry.grid(row=2, column=1, padx=5, pady=5)
+        
+        self.add_button = tk.Button(self.master, text="Add", command=self.add_data)
+        self.add_button.grid(row=3, column=0, padx=5, pady=5)
+        
+        self.update_button = tk.Button(self.master, text="Update", command=self.update_data)
+        self.update_button.grid(row=3, column=1, padx=5, pady=5)
+        
+        self.delete_button = tk.Button(self.master, text="Delete", command=self.delete_data)
+        self.delete_button.grid(row=3, column=2, padx=5, pady=5)
+        
+        self.listbox = tk.Listbox(self.master)
+        self.listbox.grid(row=4,column=0, padx=5,pady=5)
+        self.listbox.bind("<<ListboxSelect>>", self.load_data)
+        
+        self.conn = sqlite3.connect("contacts.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS contacts
+                            (id INTEGER PRIMARY KEY,
+                            name TEXT NOT NULL,
+                            email TEXT NOT NULL,
+                            phone TEXT NOT NULL)""")
+        self.conn.commit()
+        #load data from database
+        self.load_listbox()
+        
+    def load_listbox(self):
+        self.listbox.delete(0, tk.END)
+        self.cur.execute("SELECT * FROM contacts")
+        rows = self.cur.fetchall()
+        for row in rows:
+            self.listbox.insert(tk.END, row[1])
+    
+    def load_data(self, event):
+        try:
+            index = self.listbox.curselection()[0]
+            name = self.listbox.get(index)
+            self.cur.execute("SELECT * FROM contacts WHERE name=?", (name,))
+            row = self.cur.fetchone()
+            self.name_entry.delete(0, tk.END)
+            self.name_entry.insert(0, row[1])
+            self.email_entry.delete(0, tk.END)
+            self.email_entry.insert(0, row[2])
+            self.phone_entry.delete(0, tk.END)
+            self.phone_entry.insert(0, row[3])
+        except IndexError:
+            pass
+        
+    def add_data(self):
+        name = self.name_entry.get()
+        email = self.email_entry.get()
+        phone = self.phone_entry.get()
+        self.cur.execute("INSERT INTO contacts(name,email,phone) VALUES(?,?,?)", (name,email,phone))
+        self.conn.commit()
+        self.load_listbox()
+        
+    def update_data(self):
+        try:
+            index = self.listbox.curselection()[0]
+            name = self.listbox.get(index)
+            new_name = self.name_entry.get()
+            new_email = self.email_entry.get()
+            new_phone = self.phone_entry.get()
+            self.cur.execute("UPDATE contacts SET name=?, email=?, phone=? WHERE name=?",(new_name, new_email, new_phone, name))
+            self.conn.commit()
+            self.load_listbox()
+        except IndexError:
+            pass
+        
+    def delete_data(self):
+        try:
+            index = self.listbox.curselection()[0]
+            name= self.listbox.get(index)
+            self.cur.execute("DELETE FROM contacts WHERE name=?", (name,))
+            self.conn.commit()
+            self.load_listbox()
+        except IndexError:
+            pass
+    
+    def __del__(self):
+        self.conn.close()
+        
+if __name__ =="__main__":
+    root= tk.Tk()
+    app = CRUDGUI(root)
+    root.mainloop()
