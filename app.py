9d11ee50bef1a92fcc92c4216ccef6b76307f103
jvtaylar-cpe <90173488+jvtaylar-cpe@users.noreply.github.com>
2023-05-12 21:37:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 import streamlit as st
+import numpy as np
 
-# Get the first number from the user.
-first_number = st.number_input("Enter the first number:")
+# Create a header for the calculator.
+st.header("Scientific Calculator")
 
-# Get the second number from the user.
-second_number = st.number_input("Enter the second number:")
+# Create a function to perform a mathematical operation on two numbers.
+def calculate(operation, number1, number2):
+    if operation == "+":
+        return number1 + number2
+    elif operation == "-":
+        return number1 - number2
+    elif operation == "*":
+        return number1 * number2
+    elif operation == "/":
+        return number1 / number2
+    elif operation == "^":
+        return np.power(number1, number2)
+    elif operation == "log":
+        return np.log(number1)
+    elif operation == "sin":
+        return np.sin(number1)
+    elif operation == "cos":
+        return np.cos(number1)
+    elif operation == "tan":
+        return np.tan(number1)
+    elif operation == "sqrt":
+        return np.sqrt(number1)
+    else:
+        raise ValueError("Invalid operation.")
 
-# Multiply the two numbers and print the result.
-product = first_number * second_number
-st.write("The product of the two numbers is", product)
+# Create a number input for the first number.
+number1 = st.number_input("Enter the first number:")
+
+# Create a number input for the second number.
+number2 = st.number_input("Enter the second number:")
+
+# Create a dropdown menu for the mathematical operation.
+operation = st.selectbox("Select a mathematical operation:", ["+", "-", "*", "/", "^", "log", "sin", "cos", "tan", "sqrt"])
+
+# Calculate the result of the mathematical operation.
+result = calculate(operation, number1, number2)
+
+# Display the result of the mathematical operation.
+st.write("The result is", result)
